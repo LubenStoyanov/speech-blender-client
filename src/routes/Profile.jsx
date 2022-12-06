3fb2281e409e8d1dd1cbd2b6b63637f951fe019f
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-12-06 11:25:19 +0100

    Accept jpeg images

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -141,7 +141,7 @@ export default function Profile() {
                 type="file"
                 name="file"
                 id="avatar"
-                accept="image/png"
+                accept="image/png, image/jpeg"
                 required
                 style={{ display: showModal ? "" : "none" }}
               />
